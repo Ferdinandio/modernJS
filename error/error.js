55c9824d861cb87241619889b553d38d6b4e276a
Ferdinandio <dmi8829@yandex.ru>
2020-04-02 16:41:40 +0400

    error

---
+++ error/error.js
@@ -0,0 +1,23 @@
+// json = '{"id": 5}';
+
+// try {
+// 	console.log('Начинаем работу');
+// 	let user = JSON.parse(json);
+// 	console.log(json);
+
+// 	if (!user.name) {
+// 		throw new Error('В этих данных нет имени');
+// 	}
+// 	console.log('результат');
+// } catch(error) {
+// 	console.log(error.name);
+// 	console.log(error.message);
+// 	console.log(error.stack);
+
+// 	console.log(`Ошибка ${error.name} прислала сообшение: ${error.message}`);
+// } finally {
+// 	console.log('Я вывожусь всегда');	
+// }
+
+
+// console.log('А я продолжаю работать');
